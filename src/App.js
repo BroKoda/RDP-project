bc62656907dab74a372fc4d3a3c6aecd683321c4
BroKoda <kottix96@gmail.com>
2022-10-18 14:05:10 +0200

    Modal component

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ import {LargePersonListItem} from "./people/LargePersonListItem";
 import {NumberedList} from "./NumberedList";
 import {LargeProductListItem} from "./products/LargeProductListItem";
 import {SmallProductListItem} from "./products/SmallProductListItem";
-
-const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
-}, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
-}, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
-}];
+import {Modal} from "./Modal";
 
 const products = [{
 	name: 'Flat-Screen TV',
@@ -40,14 +24,12 @@ const products = [{
 	rating: 4.2,
 }];
 
-
 function App() {
 	return (
 		<>
-			{/*<RegularList items={people} resourceName={'person'} itemComponent={SmallPersonListItem} />*/}
-			{/*<RegularList items={people} resourceName={'person'} itemComponent={LargePersonListItem} />*/}
-			<NumberedList items={products} resourceName={'product'} itemComponent={LargeProductListItem} />
-			<NumberedList items={products} resourceName={'product'} itemComponent={SmallProductListItem} />
+			<Modal>
+				<LargeProductListItem product={products[0]}/>
+			</Modal>
 		</>
 	);
 }
@@ -67,5 +49,49 @@ export default App;
 // 			<LeftHandComponent name={'Konrad'}/>
 // 			<RightHandComponent message={'Helloka'}/>
 // 		</SplitScreen>
+// 	);
+// }
+
+/* ----- ----- ----- ----- ----- LIST ELEMENTS ----- ----- ----- ----- ----- */
+// const people = [{
+// 	name: 'John Doe',
+// 	age: 54,
+// 	hairColor: 'brown',
+// 	hobbies: ['swimming', 'bicycling', 'video games'],
+// }, {
+// 	name: 'Brenda Smith',
+// 	age: 33,
+// 	hairColor: 'black',
+// 	hobbies: ['golf', 'mathematics'],
+// }, {
+// 	name: 'Jane Garcia',
+// 	age: 27,
+// 	hairColor: 'blonde',
+// 	hobbies: ['biology', 'medicine', 'gymnastics'],
+// }];
+// const products = [{
+// 	name: 'Flat-Screen TV',
+// 	price: '$300',
+// 	description: 'Huge LCD screen, a great deal',
+// 	rating: 4.5,
+// }, {
+// 	name: 'Basketball',
+// 	price: '$10',
+// 	description: 'Just like the pros use',
+// 	rating: 3.8,
+// }, {
+// 	name: 'Running Shoes',
+// 	price: '$120',
+// 	description: 'State-of-the-art technology for optimum running',
+// 	rating: 4.2,
+// }];
+// function App() {
+// 	return (
+// 		<>
+// 			<RegularList items={people} resourceName={'person'} itemComponent={SmallPersonListItem} />
+// 			<RegularList items={people} resourceName={'person'} itemComponent={LargePersonListItem} />
+// 			<NumberedList items={products} resourceName={'product'} itemComponent={LargeProductListItem} />
+// 			<NumberedList items={products} resourceName={'product'} itemComponent={SmallProductListItem} />
+// 		</>
 // 	);
 // }
